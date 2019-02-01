755e16b456e4cdaaf5076bfc993c10e7abca388b
Susmit-A <susmit600@gmail.com>
2019-02-02 00:42:57 +0530

    Made sample app classes package-private

--- app/src/main/java/com/susmit/stacklayout/HorizontalLtrStackActivity.java
+++ app/src/main/java/com/susmit/stacklayout/HorizontalLtrStackActivity.java
@@ -3,7 +3,7 @@ package com.susmit.stacklayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HorizontalLtrStackActivity extends AppCompatActivity {
+class HorizontalLtrStackActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
